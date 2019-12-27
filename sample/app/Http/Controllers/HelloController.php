68ee07358901a5140fbf44be6bf673f263f11bc8
dj8639 <dj8639rsm@gmail.com>
2019-12-28 00:30:18 +0900

    validator min:0 max :200

--- sample/app/Http/Controllers/HelloController.php
+++ sample/app/Http/Controllers/HelloController.php
@@ -11,22 +11,51 @@ class HelloController extends Controller
 {
     public function index(Request $request){
 
-        return view('hello.index',['msg'=>'フォームを入力:']);
+        $validator = Validator::make($request->query(),[
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator -> fails()){
+            $msg = 'クエリーに問題があります';
+        }else{
+            $msg = 'ID,PASSを受け付けました。フォームを入力してください。';
+        } 
+
+        return view('hello.index',['msg'=>$msg, ]);
     }
 
     public function post(Request $request){
 
-        $validator = Validator::make($request->all(), [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ]);
+            'age' => 'numeric',
+        ];
 
-        if ($validator->fails()){
+        $messages = [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入してください。',
+            'age.min' => '年齢はゼロ才以上で入力してください。',
+            'age.max' => '年齢は２００才以下で入力してください。',
+        ];
+
+        $validator = validator::make($request->all(),$rules,$messages);
+
+        $validator->sometimes('age', 'min:0', function($input){
+            return !is_int($input->age);
+        });
+
+        $validator->sometimes('age','max:200',function($input){
+            return !is_int($input->age);
+        });
+
+        if($validator->fails()){
             return redirect('/hello')
-                            ->withErrors($validator)
-                            ->withInput();
+            ->withErrors($validator)
+            ->withInput();
         }
+
         return view('hello.index', ['msg'=>'正しく入力されました!']);
     }
 }
